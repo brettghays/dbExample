8427ef06f0d5ace11193e155cf2fe78dfe2fd39c
brettghays <brett.g.hays.coding@gmail.com>
2018-04-21 12:05:17 -0600

    added student table schema

--- alterTable.sql
+++ alterTable.sql
@@ -1,4 +1,16 @@
 /* Add grade column */
+create table Students (
+    student_ID INT PRIMARY KEY,
+    first_name text,
+    last_name  text
+);
+
+insert into Students (student_id, first_name, last_name) values
+(1, 'Brett', 'Hays'),
+(2, 'Megan', 'Hays'),
+(3, 'Carter', 'Hays'),
+(4, 'Kendyl', 'Hays'),
+(5, 'Hadley', 'Hays');
 
 alter table Students
 add column grade text;
